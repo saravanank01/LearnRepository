0e52725fa1847d3a6098c13fe9d654695b2f7630
Saravanan@BeginCode() <88998597+saravanank01@users.noreply.github.com>
2022-02-23 21:36:01 +0530

    delete myscript.js
    not working in w3 if mentioned the path in the URL

--- myscript.js
+++
@@ -1,4 +0,0 @@
-function myFunction(){
-document.getElementById("demo").innerHTML ="paragraph changed by using a URL from github !";
-//document.getElementById("demo").innerHTML.style.color="red";
-}
